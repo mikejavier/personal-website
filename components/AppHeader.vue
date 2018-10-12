7b358167d05be22ddbc5015e322f74034b26ee3f
Michael Santillán <contato@michaelsantillan.com>
2018-10-11 22:26:55 -0300

    Update AppHeader component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,45 +2,49 @@
   <header class="hero is-dark is-fullheight is-bold">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <img src="~/assets/images/ms.png"  alt="Michael Santillán Avatar" class="about-avatar">
-
+        <img src="~/assets/images/ms.png"  alt="Michael Santillán" class="about-avatar">
         <h1 class="title">Michael Santillán</h1>
-
-        <h2 class="subtitle">Web Developer</h2>
-
+        <h2 class="subtitle">Front-end Engineer</h2>
         <nav class="field is-grouped social-icons">
           <span class="control">
             <a href="http://br.linkedin.com/in/mikejavier/" target="_blank" class="button is-outlined is-white">
-              <span class="icon"><i class="fa fa-linkedin"></i></span>
+              <span class="icon"><i class="fab fa-linkedin-in"></i></span>
             </a>
           </span>
-
           <span class="control">
             <a href="https://github.com/mikejavier" target="_blank" class="button is-outlined is-white">
-              <span class="icon"><i class="fa fa-github"></i></span>
+              <span class="icon"><i class="fab fa-github"></i></span>
             </a>
           </span>
-
           <span class="control">
             <a href="http://www.twitter.com/_mikejavier" target="_blank" class="button is-outlined is-white">
-              <span class="icon"><i class="fa fa-twitter"></i></span>
+              <span class="icon"><i class="fab fa-twitter"></i></span>
             </a>
           </span>
-
           <span class="control">
-            <a href="http://www.facebook.com/zmike.javier" target="_blank" class="button is-outlined is-white">
-              <span class="icon"><i class="fa fa-facebook"></i></span>
+            <a href="https://medium.com/@mikejavier" target="_blank" class="button is-outlined is-white">
+              <span class="icon"><i class="fab fa-medium-m"></i></span>
+            </a>
+          </span>
+          <span class="control">
+            <a href="https://t.me/mikejavier" target="_blank" class="button is-outlined is-white">
+              <span class="icon"><i class="fab fa-telegram-plane"></i></span>
             </a>
           </span>
         </nav>
-
       </div>
     </div>
   </header>
 </template>
 
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
 <style lang="scss" scoped>
-  .social-icons{
-    justify-content: center;
-  }
+.social-icons{
+  justify-content: center;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <ms-header></ms-header>
+    <app-header></app-header>
     <main>
       <about></about>
       <skills></skills>

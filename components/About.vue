0895c014b0c7f0710e0eb81da21f378050c81232
Michael Santillán <contato@michaelsantillan.com>
2018-10-12 17:09:12 -0300

    Update About component

--- components/About.vue
+++ components/About.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <h1 class="title">About</h1>
     <div class="content">
-      <p>Hi, my name is Michael Santillán born in Ecuador, i am {{getAge}} years old and currently live in Brazil where i work as a Front End Developer. In free time i do not dispense a good video game or a trip with my wife and our shih-tzu. Passionate by remote work and open-source, i'm moved driven by curiosity and the desire to evolve more and more.</p>
+      <p>Hi, my name is Michael Santillán born in Ecuador, i am {{getAge}} years old and currently live in Brazil where i work as a Front End Engineer. In free time i do not dispense a good video game or a trip with my wife and our shih-tzu. Passionate by remote work and open-source, i'm moved driven by curiosity and the desire to evolve more and more.</p>
     </div>
   </section>
 </template>

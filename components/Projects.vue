6c72f9a46febd2bbd816b3696349e1f1d20a0e5a
Michael Santillán <contato@michaelsantillan.com>
2018-10-12 17:09:03 -0300

    Update Projects component

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,61 +1,48 @@
 <template>
   <section class="section">
-    <div class="container">
-
-      <h1 class="title">Projects</h1>
-
-      <h2 class="subtitle">In my freetime develop or contribute some projects to learn.</h2>
-
-      <div class="tabs is-boxed">
-        <ul>
-          <li :class="{'is-active': !fork}">
-            <a @click.prevent="projectsToogle('source')"><span>Developments</span></a>
-          </li>
-          <li :class="{'is-active': fork}">
-            <a @click.prevent="projectsToogle('fork')"><span>Contributions</span></a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="columns is-multiline is-tablet">
-        <div class="column is-one-quarter" v-for="(repo, index) in repos" :key="index" v-if="repo.fork === fork">
-          <article class="card">
-            <header class="card-header">
-              <p class="card-header-title">{{repo.name}}</p>
-              <a :href="repo.html_url" target="_blank" class="card-header-icon">
-                <span class="icon">
-                  <i class="fa fa-github" ></i>
-                </span>
-              </a>
-            </header>
-            <div class="card-content">
-              <div class="content">
-                <p>{{repo.description}}</p>
-                <span class="tag is-light">{{repo.language ? repo.language : 'Markdown'}}</span>
-              </div>
-            </div>
-          </article>
-        </div>
-      </div>
-    </div>
+    <h1 class="title">Open-source</h1>
+    <p>
+      In my free time I try contribute in cool projects...
+      <a
+      v-for="(repo, index) in repositories"
+      :key="index"
+      :href="repo.html_url"
+      target="_blank">
+        {{ repo.name + getDot(index) }}
+      </a>
+      Also turning crazy ideas into code
+      <a
+      href="https://github.com/mikejavier/correiosjs"
+      target="_blank">
+        correiosjs,
+      </a>
+      <a
+      href="https://github.com/mikejavier/vue-emojiarea"
+      target="_blank">
+        vue-emojiarea.
+      </a>
+      You can find more on
+      <a
+        href="https://github.com/mikejavier?tab=repositories"
+        target="_blank">GitHub</a>.
+    </p>
   </section>
 </template>
 
 <script>
 export default {
-  name: '',
-  props: ['repos'],
-  data () {
-    return {
-      fork: false,
-      activeTab: 'source'
+  name: 'Projects',
+
+  props: {
+    repositories: {
+      type: Array,
+      required: true
     }
   },
+
   methods: {
-    projectsToogle (activeTab) {
-      if (this.activeTab === activeTab) return
-      this.fork = !this.fork
-      this.activeTab = activeTab
+    getDot (index) {
+      return index === this.repositories.length - 1 ? '.' : ','
     }
   }
 }

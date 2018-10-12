4c0724c6a3b104ced2bb8570594a000c1e7e871d
Michael Santillán <contato@michaelsantillan.com>
2018-10-12 02:15:03 -0300

    Update Skills component

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,41 +1,26 @@
 <template>
-  <section class="hero is-dark">
-    <div class="hero-body">
-      <div class="container">
-
-        <h1 class="title">Skills</h1>
-
-        <h2 class="subtitle">Solid knowledge of front-end technologies and some experience with back-end.</h2>
-
-        <p class="field is-grouped" style="justify-content: space-between; flex-wrap: wrap;">
-          <span class="control is-size-2"><i class="devicon-html5-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-css3-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-javascript-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-nodejs-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-php-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-mongodb-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-mysql-plain colored"></i></span>
-          <span class="control is-size-2"><i class="devicon-wordpress-plain"></i></span>
-          <span class="control is-size-2"><i class="devicon-git-plain colored"></i></span>
-        </p>
-
-        <div class="section">
-          <div class="columns">
-            <div class="column is-half">
-              <small class="has-text-centered is-block has-text-light">Front-end</small>
-              <progress class="progress is-large is-primary" value="95" max="100">60%</progress>
-            </div>
-
-            <div class="column is-half">
-              <small class="has-text-centered is-block has-text-light">Back-end</small>
-              <progress class="progress is-large is-primary" value="40" max="100">60%</progress>
-            </div>
-          </div>
-        </div>
-
-      </div>
+  <section class="section">
+    <h1 class="title">Skills</h1>
+    <p class="field is-grouped" style="justify-content: space-between; flex-wrap: wrap;">
+      <span class="control is-size-3"><i class="devicon-html5-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-css3-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-javascript-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-vuejs-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-react-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-nodejs-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-python-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-wordpress-plain"></i></span>
+      <span class="control is-size-3"><i class="devicon-docker-plain"></i></span>
+    </p>
+    <div class="content">
+      <p>
+        <small class="has-text-weight-semibold">Front-end</small>
+        <progress class="progress is-large is-primary" value="95" max="100">60%</progress>
+      </p>
+      <p>
+        <small class="has-text-weight-semibold">Back-end</small>
+        <progress class="progress is-large is-primary" value="60" max="100">60%</progress>
+      </p>
     </div>
   </section>
 </template>
-
-<style lang="scss" scoped></style>

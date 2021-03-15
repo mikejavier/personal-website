a1c16fe997bea715d53bbc52d2b653a7c25df668
Michael Santillan <me@michaelsantillan.com>
2021-03-15 08:50:54 -0300

    Remove TheProjects component

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,13 @@
     <the-intro />
     <the-skills />
     <the-works />
-    <the-projects :repos="contributedRepos" />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import TheIntro from '@/components/TheIntro'
 import TheSkills from '@/components/TheSkills'
 import TheWorks from '@/components/TheWorks'
-import TheProjects from '@/components/TheProjects'
 
 export default {
   name: 'About',
@@ -20,35 +17,7 @@ export default {
   components: {
     TheIntro,
     TheSkills,
-    TheWorks,
-    TheProjects
-  },
-
-  async asyncData() {
-    try {
-      const repos = await axios.get(
-        'https://api.github.com/users/mikejavier/repos?client_id=f5095e6204e7bfccb7be&client_secret=9509f372612b4b1719bc3fdc465f06e6c2fb2107&per_page=100'
-      )
-      const forkedRepos = repos.data.filter(({ fork }) => fork)
-      const promises = forkedRepos.map(async repo => {
-        const { data } = await axios.get(
-          `${repo.contributors_url}?client_id=f5095e6204e7bfccb7be&client_secret=9509f372612b4b1719bc3fdc465f06e6c2fb2107`
-        )
-        repo.contributors = data
-        return repo
-      })
-
-      const forkedReposWithContributors = await Promise.all(promises)
-
-      const contributedRepos = forkedReposWithContributors.filter(
-        ({ contributors }) =>
-          contributors.some(({ login }) => login === 'mikejavier')
-      )
-
-      return { contributedRepos }
-    } catch (e) {
-      return e
-    }
+    TheWorks
   }
 }
 </script>
